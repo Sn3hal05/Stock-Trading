cc49ca9166a76c486360f6184c5d8b3d8e81b37d
Snehal <msnehalb3805@gmail.com>
2026-03-21 14:11:31 +0530

    added the hero section to the pricing page

--- frontend/src/landing_page/pricing/Hero.js
+++ frontend/src/landing_page/pricing/Hero.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <div>
-            <h1>Hero</h1>
+        <div className="container text-center my-5 py-5">
+            <h2>Charges</h2>
+            <p className='text-muted'>List of all charges and taxes</p>
+            <div className='row my-5'>
+                <div className="col-12 col-lg-4 pe-5">
+                    <img src="../media/images/pricingMF.svg" style={{width:"70%"}}/>
+                    <h1 className='fs-3'>Free equity delivery</h1>
+                    <p className="text-muted">All equity delivery investments (NSE, BSE), are absolutely free — ₹ 0 brokerage.</p>
+                </div>
+                <div className="col-12 col-lg-4 pe-5">
+                    <img src="../media/images/intradayTrades.svg" style={{width:"70%"}}/>
+                    <h1 className='fs-3'>Intraday and F&O trades</h1>
+                    <p className="text-muted">Flat ₹ 20 or 0.03% (whichever is lower) per executed order on intraday trades across equity, currency, and commodity trades. Flat ₹20 on all option trades.</p>
+                </div>
+                <div className="col-12 col-lg-4 pe-5">
+                    <img src="../media/images/pricingMF.svg" style={{width:"70%"}}/>
+                    <h1 className='fs-3'>Free direct MF</h1>
+                    <p className="text-muted">All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.</p>
+                </div>
+
+            </div>
         </div>
     )
 }
