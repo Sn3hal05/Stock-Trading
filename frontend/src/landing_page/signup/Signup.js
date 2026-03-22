0e083ed9d69bb4151a5c5dbf36d7872a4beda057
Snehal <msnehalb3805@gmail.com>
2026-03-22 12:57:24 +0530

    added flag icons package to the signup

--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Link} from 'react-router-dom';
+import IN from 'country-flag-icons/react/3x2/IN';
 
 const Signup = () => {
   return (
@@ -18,8 +19,11 @@ const Signup = () => {
         <div className="col-12 col-lg-6 mt-3">
           <h3>Signup now</h3>
           <p className="text-muted">Or track your existing application</p>
-          <input type="number"></input>
-          <br/>
+          <div className="input-group">
+            <span className="input-group-text bg-white">
+              <IN style={{ width: "20px", height: "15px" }} /> &nbsp; +91</span>
+          <input pattern="[0-9]{10}" maxLength='10' className="form-control" type="tel"></input>
+          </div>
           <br/>
           <button className='btn btn-primary px-5 fs-5'>Get OTP</button>
           <p className="text-muted fs-7 mt-3 ">By proceeding, you agree to the Zerodha <a href="#" className="text-decoration-none">terms & privacy policy</a></p>
